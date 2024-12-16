58ae6aae21b160f6001a438643adb87728c858a0
asa93 <asa93@live.fr>
2024-12-16 17:32:26 -0300

    /attetation endpoint

--- example/service.py
+++ example/service.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import time
+import urllib.request
 
 app = Flask(__name__)
+
+nitriding_url = "http://127.0.0.1:8080/enclave/ready"
+
 
 @app.route('/')
 def home():
@@ -9,6 +14,14 @@ def home():
 @app.route('/about')
 def about():
     return 'About page'
+
+@app.route('/attestation')
+def enclave_ready():
+    r = urllib.request.urlopen(nitriding_url)
+    if r.getcode() != 200:
+        return 'Enclave not ready'
+    else:
+        return 'Enclave ready'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7047)

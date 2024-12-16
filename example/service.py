38551eff46992ff2100394a6fb5182073e0709e7
asa93 <asa93@live.fr>
2024-12-16 12:25:32 -0300

    add flask server example

--- example/service.py
+++ example/service.py
@@ -1,28 +1,14 @@
-#!/usr/bin/env python3
+from flask import Flask
 
-import time
-import urllib.request
+app = Flask(__name__)
 
-nitriding_url = "http://127.0.0.1:8080/enclave/ready"
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
+@app.route('/about')
+def about():
+    return 'About page'
 
-def signal_ready():
-    r = urllib.request.urlopen(nitriding_url)
-    if r.getcode() != 200:
-        raise Exception("Expected status code %d but got %d" %
-                        (requests.status_codes.codes.ok, r.status_code))
-
-
-def fetch_addr():
-    url = "https://raw.githubusercontent.com/brave/nitriding-daemon/master/README.md"
-    with urllib.request.urlopen(url) as f:
-        print("[py] Fetched %d bytes of README.md." % len(f.read(100)))
-
-
-if __name__ == "__main__":
-    signal_ready()
-    print("[py] Signalled to nitriding that we're ready.")
-
-    time.sleep(1)
-    fetch_addr()
-    print("[py] Made Web request to the outside world.")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7047)
--- example/start.sh
+++ example/start.sh
@@ -5,5 +5,11 @@ echo "[sh] Started nitriding."
 
 sleep 1
 
-service.py
+
+cd bin
+
+pip3 install flask
+
+python3 service.py
+
 echo "[sh] Ran Python script."

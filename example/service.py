150b499aab828422d084ded7c28a199f8b7604e8
asa93 <asa93@live.fr>
2024-12-16 20:47:12 -0300

    port

--- example/service.py
+++ example/service.py
@@ -73,4 +73,4 @@ def callmodel():
 if __name__ == "__main__":
     signal_ready()
     print("[py] Signalled to nitriding that we're ready.")
-    app.run(host="0.0.0.0", port=7046)
+    app.run(host="0.0.0.0", port=7047)
